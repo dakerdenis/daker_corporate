9bddbf6c8160159891fee8aed3f2123f6bfe9cca
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-12-13 14:55:47 +0400

    added

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daker - Software Digital Studio</title>
+
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    
+    <div class="main__wrapper">
+        <!---image---->
+        <div class="main__wrapper__placeholder">
+
+        </div>
+
+        <!---image---->
+        <div class="main__wrapper__placeholder">
+
+        </div>
+
+        <!---image---->
+        <div class="main__wrapper__placeholder">
+
+        </div>
+
+        <!---long image---->
+        <div class="main__wrapper__placeholder">
+
+        </div>    
+
+
+        <!---REST CONTAINER---->
+        <div class="content__container">
+            <!----MAIN container----->
+            <div class="content__main__block">
+
+            </div>
+
+            <!---SERVICES----->
+            <div class="content__whatWeDo">
+
+            </div>
+
+            <!-----PORTFOLIO ---->
+            <div class="content__portfolio">
+
+            </div>
+
+
+            <!----OUR CLIENTS--->
+            <div class="content__clients">
+
+            </div>
+
+
+
+            <!----- WHY to CHOOSE US---->
+            <div class="content__whyWe">
+
+            </div>
+
+
+
+            <!---OUR VISION--->
+            <div class="content__ourVision">
+
+            </div>
+
+
+            <!----TEAM MEMBERS--->
+
+        </div>
+
+    </div>
+    
+
+<script src="./js/app.js"></script>
+</body>
+</html>
