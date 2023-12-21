febcf6216da68617e761e1fa157d70eda5643b49
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-12-21 16:34:10 +0400

    added

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,9 @@
 
     <link rel="stylesheet" href="./style/style.css">
 </head>
+
 <body>
-    
+
     <div class="main__wrapper">
         <!---image---->
         <div class="main__wrapper__placeholder">
@@ -28,13 +30,30 @@
         <!---long image---->
         <div class="main__wrapper__placeholder">
 
-        </div>    
+        </div>
 
 
         <!---REST CONTAINER---->
         <div class="content__container">
             <!----MAIN container----->
             <div class="content__main__block">
+                <!---LOGO--->
+                <div class="__main__block__logo">
+                    <div class="__main__block__image">
+                        <img src="./style/imgs/logo_big.png" alt="">
+                    </div>
+                </div>
+                <!---main text content----->
+                <div class="__main__block__text">
+                    <div class="__main__block__text_container">
+                        <p>WE PROVIDE</p>
+                        <p>MORE OPPORTUNITIES</p>
+                        <p>FOR YOUR BUSINESS</p>
+                        <p>WITH CUTTING-EDGE</p>
+                        <img src="./style/imgs/web_solutions.png" alt="">
+                    </div>
+                </div>
+                <!-----social block----->
 
             </div>
 
@@ -71,14 +90,15 @@
 
             <!----TEAM MEMBERS--->
             <div class="content__team">
-                
+
             </div>
 
         </div>
 
     </div>
-    
 
-<script src="./js/app.js"></script>
+
+    <script src="./js/app.js"></script>
 </body>
+
 </html>
