a2a504b8f2be46b2c2c3f558ecbd3f3ae171ad74
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-01-03 17:42:39 +0300

    added

--- js/app.js
+++ js/app.js
@@ -17,3 +17,66 @@ function openTab(tabName) {
     document.querySelector('.tab[data-tab="' + tabName + '"]').classList.add("active");
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var modal = document.getElementById('imageModal');
+  var modalImage = document.getElementById('modalImage');
+  var openImages = document.getElementsByClassName('openImage');
+  
+  // Array to store image sources
+  var imageSources = [
+    "./style/imgs/seit1.png",
+    "./style/imgs/seit2.png",
+    // Add more image sources as needed
+  ];
+  
+  // Loop through each image and attach a click event
+  for (var i = 0; i < openImages.length; i++) {
+    openImages[i].addEventListener('click', function () {
+      // Find the index of the clicked image
+      var index;
+      for (var j = 0; j < openImages.length; j++) {
+        if (openImages[j] === this) {
+          index = j;
+          break;
+        }
+      }
+  
+      // Set the source of the modal image based on the index
+      modalImage.src = imageSources[index];
+  
+      modal.style.display = 'flex';
+      document.body.style.overflow = 'hidden'; // Disable scrolling on the page
+  
+      console.log("Modal opened with image source:", modalImage.src);
+    });
+  }
+  
+  // Function to close the modal
+  function closeModal() {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Enable scrolling on the page
+  
+    console.log("Modal closed");
+  }
+  
+  // Close the modal if the user clicks outside of the modal content
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  };
+  
+
