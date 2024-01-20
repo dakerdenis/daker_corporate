2e40c87a1e6b2fd86498ceb1b32082d493d1f7f2
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-01-20 20:18:52 +0300

    added

--- js/app.js
+++ js/app.js
@@ -46,3 +46,66 @@ window.onclick = function (event) {
         closeModal();
     }
 };
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerIcon = document.querySelector('.header__circle__burger');
+  const expandedBlockContainer = document.getElementById('expandedBlockContainer');
+
+  // Function to toggle the block (open/close)
+  function toggleBlock() {
+      if (expandedBlockContainer.style.width === '300px') {
+          collapseBlock();
+      } else {
+          expandBlock();
+      }
+  }
+
+  // Function to expand the block
+  function expandBlock() {
+      expandedBlockContainer.style.width = '300px';
+  }
+
+  // Function to collapse the block
+  function collapseBlock() {
+      expandedBlockContainer.style.width = '0';
+  }
+
+  // Function to handle smooth scrolling to the target section
+  function scrollToSection(target) {
+      const section = document.getElementById(target);
+
+      if (section) {
+          const offset = 20; // Set your desired offset value
+          const scrollPosition = section.offsetTop - offset;
+
+          window.scrollTo({
+              top: scrollPosition,
+              behavior: 'smooth'
+          });
+      }
+  }
+
+  // Click event listener on the document
+  document.addEventListener('click', function (event) {
+      // Check if the clicked element is the burger icon or its child image
+      if (event.target === burgerIcon || burgerIcon.contains(event.target)) {
+          toggleBlock(); // Toggle the block (open/close)
+      } else {
+          // Check if the clicked element is a link inside the expanded block
+          const targetSection = event.target.getAttribute('data-target');
+          if (targetSection) {
+              scrollToSection(targetSection);
+              collapseBlock(); // Collapse the block after clicking a link
+          }
+      }
+  });
+});
